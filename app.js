baf370cc396053c96436c33e0190fb6f3f12317b
fabiodaemon <fabiodaemon@yahoo.com.br>
2020-01-08 22:40:15 -0300

    criando um servidor entregando htmls

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 var http = require('http');
 var ls = require('fs');
+var url = require('url');
 
 var server = http.createServer(function(req, resp){
+    var url_parts = url.parse(req.url);
+    var path = url_parts.pathname;
 
-    ls.readFile('index.html', function(err, data){
-        resp.writeHead(200, {'Content-Type': 'text/html'});
-        resp.write(data);
+    ls.readFile(__dirname + path, function(err, data){
+        if(err){
+            resp.writeHead(404, {'Content-Type': 'text/html'});
+            resp.write('Not found');
+        }else{
+            resp.writeHead(200, {'Content-Type': 'text/html'})
+            resp.write(data);
+        }
         resp.end();
     })
 });
