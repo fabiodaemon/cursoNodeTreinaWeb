5d58c35319228431f321128a78ff31831cd1ea62
fabiodaemon <fabiodaemon@yahoo.com.br>
2020-01-08 22:15:32 -0300

    trabalhando com callbacks e promises

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 var fs = require('fs');
+Promise = require('promise');
 
-// console.time('Assincrono');
-// var counter = 0;
-
-// for(var i = 0; i < 1000; i++){
-//     fs.readFile('my_file.txt',function(err, data){
-//         if(err){
-//             return console.error(err);
-//         }
-//         counter++;
-//         console.log('Assincrono ' + data.toString());
-//         if(counter === 1000){
-//             console.timeEnd('Assincrono');
-//         }
-//     })
-// }
-
-// 196.310 ms de resposta no acima
-
-console.time('Sincrono');
-var counter = 0;
-
-for(var i = 0; i < 1000; i++){
-    var data = fs.readFileSync('my_file.txt');
-    console.log('Sincrono ' + data);
+function read(file){
+    return new Promise(function(fulfill, reject){
+        fs.readFile(file, function(err, data){
+            if(err){
+                reject();
+            }else{
+                fulfill(data.toString());
+            }
+        })
+    });
 }
-    console.timeEnd('Sincrono');
 
-// 114.557 ms levou o sincrono
+read('my_file.txt')
+    .then((data) => {
+        console.log(data);
+        return '1111';
+    })
+    .then((data) => {
+        console.log(data);
+        return '2222';
+    })
+    .done((data) => {
+        console.log(data);
+    })
 
+    // Exemplo abaixo criado como seria com callbacks
+    read('my_file.txt', function(data1,callback1){
+        console.log(data);
+        callback1('1111', function(data2, callback2){
+            console.log(data2);
+            callback2('2222')
+        })
+    })
 
