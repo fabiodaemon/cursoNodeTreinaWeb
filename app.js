6578ebae724761b49ebdf7fd9eaa4b044e10e523
fabiodaemon <fabiodaemon@yahoo.com.br>
2020-01-08 22:33:06 -0300

    gerando resposta atraves de arquivo externo - usando o readFile

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 var http = require('http');
+var ls = require('fs');
 
 var server = http.createServer(function(req, resp){
-    resp.writeHead(200, {'Content-Type': 'text/html'});
 
-    resp.write('<h1>Hello World!</h1>');
-
-    resp.end();
+    ls.readFile('index.html', function(err, data){
+        resp.writeHead(200, {'Content-Type': 'text/html'});
+        resp.write(data);
+        resp.end();
+    })
 });
 
 server.listen(3000);
